7170bf9750c2ea47bb7b886189036640e4f2a99d
FFA911 <K.Tisha3@gmail.com>
2016-02-28 14:07:41 +0300

    Создание вертикальных линий. Создание рамки из линий. Изучение наследования классов (вертикальные и горизонтальные линии наследуют значения от класса фигура и могут дополняться доп параметрами)

--- Snakes/Snakes/Program.cs
+++ Snakes/Snakes/Program.cs
@@ -10,15 +10,20 @@ namespace Snakes
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');         
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');           
+            HorizontalLine Upline = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine Downline = new HorizontalLine(0, 78, 24, '+');
+            VertikalLine Leftline = new VertikalLine(0, 24, 0, '+');
+            VertikalLine Rightline = new VertikalLine(0, 24, 78, '+');
+            Upline.Drow();
+            Downline.Drow();
+            Rightline.Drow();
+            Leftline.Drow();
+
+            Point p2 = new Point(4, 5, '*');           
             p2.Draw();
-
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
-
+          
             Console.ReadLine();
         }
     }
